5078f88eb98dc43c83a1224ae6c9bcb8d264084b
unknown <tranminhtri10213@gmail.com>
2024-04-23 20:00:51 +0700

    compelete ADD feature

--- lib/src/modules/features/addStudent/addStudentPage.dart
+++ lib/src/modules/features/addStudent/addStudentPage.dart
@@ -1,8 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class AddStudentPage extends StatelessWidget {
-  const AddStudentPage({super.key});
-
+  AddStudentPage({super.key});
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController classController = TextEditingController();
+  final TextEditingController idController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,6 +17,7 @@ class AddStudentPage extends StatelessWidget {
         child: Column(
           children: [
             TextField(
+              controller: nameController,
               onChanged: (value) {
                 // text=value;
               },
@@ -24,6 +28,7 @@ class AddStudentPage extends StatelessWidget {
             ),
             const SizedBox(height: 20),
             TextField(
+              controller: classController,
               onChanged: (value) {
                 // text=value;
               },
@@ -34,6 +39,7 @@ class AddStudentPage extends StatelessWidget {
             ),
             const SizedBox(height: 20),
             TextField(
+              controller: idController,
               onChanged: (value) {
                 // text=value;
               },
@@ -47,7 +53,51 @@ class AddStudentPage extends StatelessWidget {
               width: double.infinity,
               height: 50,
               child: ElevatedButton(
-                onPressed: () {},
+                onPressed: () async {
+                  String name = nameController.text.trim();
+                  String className = classController.text.trim();
+                  String id = idController.text.trim();
+
+                  // Kiểm tra nếu có bất kỳ trường nào trống
+                  if (name.isEmpty || className.isEmpty || id.isEmpty) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Vui lòng điền đầy đủ thông tin'),
+                        duration: Duration(seconds: 2),
+                      ),
+                    );
+                  } else {
+                    // Nếu thông tin đầy đủ, tiến hành thêm sinh viên
+                    FirebaseFirestore firestore = FirebaseFirestore.instance;
+                    try {
+                      await firestore.collection('student').add({
+                        'name': name,
+                        'classname': className,
+                        'id': id
+                      });
+                      // Xóa nội dung của các TextField sau khi thêm sinh viên
+                      nameController.clear();
+                      classController.clear();
+                      idController.clear();
+                      // Hiển thị snackbar thông báo thành công
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('Thêm sinh viên thành công'),
+                          duration: Duration(seconds: 2),
+                        ),
+                      );
+                    } catch (e) {
+                      // Xử lý nếu có lỗi xảy ra
+                      print('Error: $e');
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('Đã xảy ra lỗi, vui lòng thử lại sau'),
+                          duration: Duration(seconds: 2),
+                        ),
+                      );
+                    }
+                  }
+                },
                 style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
                 child: const Text('Thêm sinh viên'),
               ),
